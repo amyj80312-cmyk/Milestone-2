7559a8f0a5623949e9e7f1971de9486eed1b22c1
ahmedkhlearn-alt <ahmed.khlearn@gmail.com>
2026-04-24 16:46:41 +0300

    hmmm

--- src/game/engine/cards/ConfusionCard.java
+++ src/game/engine/cards/ConfusionCard.java
@@ -1,5 +1,8 @@
 package game.engine.cards;
 
+import game.engine.Role;
+import game.engine.monsters.Monster;
+
 public class ConfusionCard extends Card {
 	private int duration;
 	
@@ -11,5 +14,15 @@ public class ConfusionCard extends Card {
 	public int getDuration() {
 		return duration;
 	}
+	public void performAction(Monster player, Monster opponent)
+	{
+		Role playerRole=player.getRole();
+		Role opponentRole=opponent.getRole();
+		player.setRole(opponentRole);
+		opponent.setRole(playerRole);
+		player.setConfusionTurns(duration);
+		opponent.setConfusionTurns(duration);
+	}
+
 
 }
