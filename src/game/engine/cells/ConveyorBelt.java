9dd036a08f1beac7a8933c218d6897412f36f0a6
ahmedkhlearn-alt <ahmed.khlearn@gmail.com>
2026-04-27 21:03:58 +0300

    hmmm

--- src/game/engine/cells/ConveyorBelt.java
+++ src/game/engine/cells/ConveyorBelt.java
@@ -1,9 +1,16 @@
 package game.engine.cells;
 
+import game.engine.monsters.Monster;
+
 public class ConveyorBelt extends TransportCell {
 
 	public ConveyorBelt(String name, int effect) {
 		super(name, effect);
 	}
-
+	
+	public void transport(Monster monster)
+	{
+		monster.move(getEffect());
+	}
+	//doesnt need to override it van just take from super
 }
--- src/game/engine/cells/DoorCell.java
+++ src/game/engine/cells/DoorCell.java
@@ -1,7 +1,10 @@
 package game.engine.cells;
 
+import java.util.ArrayList;
+import game.engine.Board;
 import game.engine.Role;
 import game.engine.interfaces.CanisterModifier;
+import game.engine.monsters.Monster;
 
 public class DoorCell extends Cell implements CanisterModifier {
 	private Role role;
@@ -29,6 +32,43 @@ public class DoorCell extends Cell implements CanisterModifier {
 
 	public void setActivated(boolean isActivated) {
 		this.activated = isActivated;
+	}
+	
+	public void modifyCanisterEnergy(Monster monster, int canisterValue)
+	{
+		monster.alterEnergy(canisterValue);
+	}
+	public void onLand(Monster landingMonster, Monster opponentMonster)
+	{
+		super.onLand(landingMonster, opponentMonster);
+		if(isActivated())
+			return;
+		setActivated(true);
+		ArrayList <Monster> stationaryMonsters = Board.getStationedMonsters();
+		int size = stationaryMonsters.size();
+		if(landingMonster.getRole()==role)
+		{
+			modifyCanisterEnergy(landingMonster,energy);
+			for(int i=0;i<size;i++)
+			{
+				if(stationaryMonsters.get(i).getRole()==role)
+				{
+					modifyCanisterEnergy(stationaryMonsters.get(i),energy);
+				}
+			}
+		}
+		else
+		{
+			modifyCanisterEnergy(landingMonster,-energy);
+			for(int i=0;i<size;i++)
+			{
+				if(stationaryMonsters.get(i).getRole()==landingMonster.getRole())
+				{
+					modifyCanisterEnergy(stationaryMonsters.get(i),-energy);
+				}
+			}
+
+		}
 	}
 
 }
